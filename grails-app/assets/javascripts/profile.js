57eb6d6698f5c3b477070c4e62fdf249b4a81877
Joel Cochran <joelforjava@outlook.com>
2018-10-11 22:35:11 -0400

    Refactoring JS

--- grails-app/assets/javascripts/profile.js
+++ grails-app/assets/javascripts/profile.js
@@ -1,75 +1,81 @@
-$(document).ready(function() {
-    var $followingButton = $("#followingButton");
-    var $unfollowButton = $("#unfollowButton");
-    var $username = $("#profileUsername").val();
-    $("#message, #error").hide();
-    $("#followingButton.nofollow").click(function() {
-        $followingButton.html('<div class="loader">Loading...</div>');
-        jQuery.ajax({
-            type:'POST',
-            data: {'usernameToFollow': $username},
-            url:'/user/follow',
-            success: function(data, status) {
-                displaySuccess(data);
-                $followingButton.removeClass("btn-info nofollow")
-                    .addClass("btn-default following disabled")
-                    .text("Following")
-                    .blur();
-            },
-            error: function(XMLHttpRequest,textStatus,errorThrown) {
-                displayError(XMLHttpRequest.responseText);
-            }
-        });
-        return false;
-    });
-    $("#blockButton").click(function() {
-        jQuery.ajax({
-            type:'POST',
-            data:{'usernameToBlock': $username},
-            url:'/user/block',
-            success: function(data,textStatus) {
-                displaySuccess(data);
+(function($, window, document) {
 
+    $(function() {
+        var $followingButton = $("#followingButton");
+        var $unfollowButton = $("#unfollowButton");
+        var $blockButton = $("#blockButton");
+        var $username = $("#profileUsername").val();
+        $("#message, #error").hide();
+        $("#followingButton.nofollow").click(function() {
+            $followingButton.html('<div class="loader">Loading...</div>');
+            $.ajax({
+                type:'POST',
+                data: {'usernameToFollow': $username},
+                url:'/user/follow',
+                success: function(data, status) {
+                    displaySuccess(data);
+                    $followingButton.removeClass("btn-info nofollow")
+                        .addClass("btn-default following disabled")
+                        .text("Following")
+                        .blur();
+                },
+                error: function(XMLHttpRequest,textStatus,errorThrown) {
+                    displayError(XMLHttpRequest.responseText);
+                }
+            });
+            return false;
+        });
+        $blockButton.click(function() {
+            blockUser($username).done(function(data) {
+                displaySuccess(data);
                 $("#optionsMenu").blur();
-            },
-            error: function(XMLHttpRequest,textStatus,errorThrown) {
-                displayError(XMLHttpRequest.responseText);
-            }
+            }).catch(function (err) {
+                displayError(err);
+            });
         });
-        return false;
-    });
-    $unfollowButton.click(function() {
-        $followingButton.html('<div class="loader">Loading...</div>');
-        $(".dropdown-menu").blur();
-        jQuery.ajax({
-            type:'POST',
-            data:{'usernameToUnfollow': $username},
-            url:'/user/unfollow',
-            success: function(data,textStatus) {
-                displaySuccess(data);
-                $("#unfollowButton, #optionsMenu").blur();
-                $followingButton.addClass("btn-info nofollow")
-                    .removeClass("btn-default following disabled")
-                    .html('<span class="glyphicon glyphicon-plus"></span> Follow Me!')
-                    .blur();
-                $unfollowButton.hide();
-            },
-            error: function(XMLHttpRequest,textStatus,errorThrown) {
-                displayError(XMLHttpRequest.responseText);
-            }
+        $unfollowButton.click(function() {
+            $followingButton.html('<div class="loader">Loading...</div>');
+            $(".dropdown-menu").blur();
+            $.ajax({
+                type:'POST',
+                data:{'usernameToUnfollow': $username},
+                url:'/user/unfollow',
+                success: function(data,textStatus) {
+                    displaySuccess(data);
+                    $("#unfollowButton, #optionsMenu").blur();
+                    $followingButton.addClass("btn-info nofollow")
+                        .removeClass("btn-default following disabled")
+                        .html('<span class="glyphicon glyphicon-plus"></span> Follow Me!')
+                        .blur();
+                    $unfollowButton.hide();
+                },
+                error: function(XMLHttpRequest,textStatus,errorThrown) {
+                    displayError(XMLHttpRequest.responseText);
+                }
+            });
+            return false;
         });
-        return false;
+
+        function blockUser(username) {
+            var data = { usernameToBlock : username };
+            return $.ajax({
+                type: 'POST',
+                data: data,
+                url: '/user/block'
+            });
+        }
+
+        function displaySuccess(message) {
+            $("#message").html(message)
+                .show("fast")
+                .fadeOut(10000);
+        }
+
+        function displayError(message) {
+            $("#error").html(message)
+                .show("fast")
+                .fadeOut(10000);
+        }
     });
 
-    function displaySuccess(message) {
-        $("#message").html(message)
-            .show("fast")
-            .fadeOut(10000);
-    }
-
-    function displayError(message) {
-        $("#error").html(message)
-            .show("fast")
-            .fadeOut(10000);
-    }
-});
+}(window.jQuery, window, document));
--- grails-app/assets/javascripts/ripples.js
+++ grails-app/assets/javascripts/ripples.js
@@ -1,41 +1,45 @@
-$(document).ready(function() {
-	var $addTopicBtn = $("#addTopicBtn");
-	var $topicContent = $("#topicContent");
-	$addTopicBtn.click(function() {
-		showSpinner(true);
-        $topicContent.fadeTo("fast", 0.4);
-		$(this).attr('disabled', 'disabled');
-		jQuery.ajax({
-			type:'POST',
-			data:jQuery(this).parents('form:first').serialize(), 
-			url:'/ripple/addAjax',
-			success: function(data,textStatus){
-				$('#allTopics').html(data);
-				clearTopic(data);
-			},
-			error: function(XMLHttpRequest,textStatus,errorThrown){
-			},
-			complete: function(XMLHttpRequest,textStatus){
-				showSpinner(false);
-                $topicContent.fadeTo("fast", 1.0);
-                $addTopicBtn.removeAttr('disabled');
-                $addTopicBtn.blur();
-			}
-		});
-		return false;
-	});
-});
-function clearTopic(e) {
-	$('#topicContent').val('');
-}
-function showSpinner(visible) {
-	if (visible) {
-		console.log("Showing stuff...");
-		//$("#newPost textArea, #newPost .btn").attr('disabled', 'disabled');
-		$('.spinner').slideDown();
-	} else {
-		console.log("Hiding stuff...");
-		//$("#newPost textArea, #newPost .btn").attr('disabled', '');
-		$('.spinner').slideUp();
-	}
-}
+(function($, window, document) {
+    $(function() {
+        var $addTopicBtn = $("#addTopicBtn");
+        var $topicContent = $("#topicContent");
+        $addTopicBtn.click(function() {
+            showSpinner(true);
+            $topicContent.fadeTo("fast", 0.4);
+            $(this).attr('disabled', 'disabled');
+            $.ajax({
+                type:'POST',
+                data:jQuery(this).parents('form:first').serialize(),
+                url:'/ripple/addAjax',
+                success: function(data,textStatus){
+                    $('#allTopics').html(data);
+                    clearTopic(data);
+                },
+                error: function(XMLHttpRequest,textStatus,errorThrown){
+                },
+                complete: function(XMLHttpRequest,textStatus){
+                    showSpinner(false);
+                    $topicContent.fadeTo("fast", 1.0);
+                    $addTopicBtn.removeAttr('disabled');
+                    $addTopicBtn.blur();
+                }
+            });
+            return false;
+        });
+    });
+
+    function clearTopic(e) {
+        $('#topicContent').val('');
+    }
+    function showSpinner(visible) {
+        if (visible) {
+            console.log("Showing stuff...");
+            //$("#newPost textArea, #newPost .btn").attr('disabled', 'disabled');
+            $('.spinner').slideDown();
+        } else {
+            console.log("Hiding stuff...");
+            //$("#newPost textArea, #newPost .btn").attr('disabled', '');
+            $('.spinner').slideUp();
+        }
+    }
+
+}(window.jQuery, window, document));
